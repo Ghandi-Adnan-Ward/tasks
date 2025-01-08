1e44cd03770e10383ba57a2222f27496cde6a8b0
ghandi ward <ghandi.ward81@gmail.com>
2025-01-08 12:48:12 +0300

    edit gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root=ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   
   <React.StrictMode>
-    <Router>
+    <Router basename="/tasks">
       <App/>
       </Router>
    </React.StrictMode>,
