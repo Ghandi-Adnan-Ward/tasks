7b55f615a2e658d75882d182bfca2dce564fa75c
ghandi ward <ghandi.ward81@gmail.com>
2025-02-10 23:00:41 +0300

    fix1

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { BrowserRouter as Router,   } from "react-router-dom";
+import { HashRouter, BrowserRouter as Router,   } from "react-router-dom";
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 const root=ReactDOM.createRoot(document.getElementById('root'))
@@ -12,9 +12,9 @@ root.render(
   <React.StrictMode>
          <I18nextProvider i18n={i18n}>
 
-    <Router basename="/">
+    <HashRouter basename="/">
       <App/>
-      </Router>
+      </HashRouter>
       </I18nextProvider>
 
    </React.StrictMode>,
